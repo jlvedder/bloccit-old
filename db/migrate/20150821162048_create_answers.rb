8fe8f323ff86fced2aa53d05b3032e7aa15fcbb8
Jonathan <jlvedder@aol.com>
2015-09-02 08:33:48 -0700

    assignment 30 done

--- db/migrate/20150821162048_create_answers.rb
+++ db/migrate/20150821162048_create_answers.rb
@@ -1,7 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.body :Question
+      t.text :body
+      t.references :question, index: true, foreign_key: true
 
       t.timestamps null: false
     end
